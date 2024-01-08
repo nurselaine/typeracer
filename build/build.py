2bd95927893150bd786637e9abde410a0ada2182
michaeltorkaman <michael.torkaman.t@gmail.com>
2024-01-08 11:13:08 -0800

    changed file structure and python build was fixed. Branch depricated. move to m1elaine

--- build/build.py
+++ build/build.py
@@ -1,15 +1,30 @@
 import subprocess
 import os
 
-server_path = "./client/src/Client.java"
+client_path = "../client/src/"
+client_src = "Client.java"
+client_exec = "Client.class"
 
 script_dir = os.path.dirname(__file__) 
-absolute_path_to_java_file = os.path.join(script_dir, server_path)
+
+path_to_src = os.path.join(script_dir, client_path, client_src)
+
+path_to_exec = os.path.join(script_dir, client_path, client_exec)
+
+print(path_to_src)
+print(path_to_exec)
 
 try:
-    result = subprocess.run("javac", absolute_path_to_java_file)
-    print("Compiled!")
-
+    result = subprocess.run(["javac", path_to_src], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print("Compilation successful")
 except subprocess.CalledProcessError as e:
-    print("ooopsi")
+    print("Compilation failed")
     print(e.stdout)
+    print(e.stderr)
+
+try:
+    result = subprocess.run(["javac", path_to_exec], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+except subprocess.CalledProcessError as e:
+    print("Execution failed")
+    print(e.stdout)
+    print(e.stderr)
---
+++ client/src/build_client.py
@@ -0,0 +1,10 @@
+import os
+import subprocess 
+
+script_dir = os.path.dirname(__file__) 
+
+exec_path = "Client"
+compile_path = "Client.java"
+
+
+
