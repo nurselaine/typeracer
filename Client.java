85240b75bef3ac8fabd6af3fd7fe829e95891a64
nurselaine <Elaine.t.huynh@gmail.com>
2024-01-09 15:55:31 -0800

    Server runs with single client and continues to accept client connection after disconnect

--- Client.java
+++ Client.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 import java.io.*;
 import java.nio.Buffer;
 
-public class Client{
+public class Client {
     public static void main(String[] args) {
 
         try {
@@ -14,22 +14,26 @@ public class Client{
             // Once server accept the connection with client will socket object be created
             Socket soc = new Socket("localhost", 9001);
 
-            // System.in is an inputstream obj that takes a bytestream of data
-            // Using inputstream reader, it takes a bytestream and returns a character stream
-            // Lastly bufferedreader will be able to read the entire string from the input
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Enter your message: ");
-            String userStr = userInput.readLine(); // this method will capture user input
+            while(soc.isConnected()){
+                // System.in is an inputstream obj that takes a bytestream of data
+                // Using inputstream reader, it takes a bytestream and returns a character stream
+                // Lastly bufferedreader will be able to read the entire string from the input
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("Enter Message: ");
+                String userStr; // this method will capture user input
 
-            // printwriter obj will write any kind of data and passing in socket obj
-            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+                while((userStr = userInput.readLine()) != null){
+                    // printwriter obj will write any kind of data and passing in socket obj
+                    PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+                    // send string to server
+                    out.println(userStr);
 
-            // send string to server
-            out.println(userStr);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                    System.out.println(in.readLine());
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-            System.out.println(in.readLine());
-
+                    System.out.print("Enter Message: ");
+                }
+            }
 
         } catch (Exception e){
             e.printStackTrace();
--- Server.java
+++ Server.java
@@ -2,31 +2,59 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/*
+ * When the server gets a new connection request from client.
+ * Server will then be in an endless loop servicing this client
+ * until the client has disconnected.
+ *
+ * Once the client has disconnected, the server will then go
+ * back to accepting a new connection.
+ * This model is a single threaded server that can only
+ * handle one client at a time.
+ *
+ * The client program should Establish a connection to the server
+ * Execute the Connect call Sleep for a random amount of time
+ * between 1 and 10 seconds. Execute the Disconnect call
+ * */
+
 public class Server{
     public static void main(String[] args) {
 
-        System.out.println("waiting for client...");
+        System.out.println("Single threaded server...");
 
-        try {
+        try (ServerSocket ss = new ServerSocket(9001)) {
             // ServerSocket obj awaits client connectiona and creates socket obj when
             // client triggers connection request
-            ServerSocket ss = new ServerSocket(9001);
-            Socket soc = ss.accept(); // server accpts connection and captures socket obj (soc)
 
-            // receive string from client (input stream is reading data)
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-            String clientStr = in.readLine();
+            while(true){
 
-            // Send message to client output stream is from sending (writing) data to client
-            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+                try{
+                    Socket soc = ss.accept(); // server accpts connection and captures socket obj (soc)
+                    System.out.println("Accepted connection from: " + soc.getInetAddress());
 
-            out.println("Server says:" + clientStr);
+                    // receive string from client (input stream is reading data)
+                    try (BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()))) {
+                        String clientStr;
 
-            System.out.println("Socket terminating...");
+                        // when server gets new server connection, server should begin endless loop
+                        while((clientStr = in.readLine()) != null){
+                            System.out.println("Message received from client1: " + clientStr);
+
+                            // Send message to client output stream is from sending (writing) data to client
+                            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+                            out.println("Client " + soc.getInetAddress() + ": " + clientStr);
+                        }
+                    } catch (Exception e){
+                        System.out.print("Client abruptly disconnected...");
+                        e.getMessage();
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
         } catch (Exception exception){
             exception.printStackTrace();
         }
-
 
     }
 }
