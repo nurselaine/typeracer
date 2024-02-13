38b32430006bc4e0414271c59b64eed4486a0782
nurselaine <Elaine.t.huynh@gmail.com>
2024-02-13 12:14:29 -0800

    Implement ConnectRPC handler with multithreading in server

--- Server.java
+++ Server.java
@@ -1,3 +1,5 @@
+import context.UserContext;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -25,23 +27,34 @@ public class Server{
         System.out.println("Single threaded server...");
         int PORT = 3001;
 
-        ServerSocketService socketServer = new ServerSocketService(PORT);
+        try {
+            ServerSocketService socketServer = new ServerSocketService(PORT);
+            // create thread pool with 4 threads
+            ExecutorService executorService = Executors.newFixedThreadPool(4);
 
-        // create thread pool with 4 threads
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
+            while(socketServer.isAccepting()){
 
-        while(socketServer.isAccepting()){
+                // socket accepts client request & creates client socket
+                Socket clientSocket = socketServer.acceptConnection();
 
+                // Use a thread from threadpool to notify client of connection status
+                // and listen to incoming client messages
+                executorService.execute(() -> {
+                    ConnectRPC(clientSocket);
+                    receiveMessage(clientSocket);
+                });
 
-
-            Socket clientSocket = socketServer.acceptConnection();
-            System.out.println("Client successfully connected to server!");
-
-            // create new thread object
-            ClientHandler clientThread = new ClientHandler(executorService, clientSocket);
-            receiveMessage(clientSocket);
+                // create new thread object
+//            ClientHandler clientThread = new ClientHandler(executorService, clientSocket);
+            }
+        } catch (Exception e) {
+            System.out.println("Error starting server " + e.getMessage());
         }
 
+    }
+
+    private static void handleNewClient(Socket clientSocket){
+        UserContext newClient = new UserContext();
     }
 
     private static void receiveMessage(Socket clientSocket){
@@ -50,10 +63,10 @@ public class Server{
                     new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String clientMessage;
 
+            // continuously check for incoming messages and print to server
             while((clientMessage = clientInputStream.readLine()) != null){
-                System.out.println("Message recieved from client! " + clientMessage + " : " + clientSocket.getInetAddress());
-
-                sendMessage(clientSocket);
+                System.out.println("Message recieved from client! " +
+                        clientMessage + " : " + clientSocket.getInetAddress());
             }
         } catch (IOException e) {
             System.out.println("Client disconnected abruptly - " +
@@ -61,11 +74,30 @@ public class Server{
         }
     }
 
-    private static void sendMessage(Socket clientSocket){
+    /**
+     * Server Thread will handle notifying clientside that client has been connected
+     * succesfully and then transfer over to ClientHandler thread for auth RPCs
+     * */
+    private static void ConnectRPC(Socket clientSocket){
         try {
             PrintWriter serverOutputStream =
                     new PrintWriter(clientSocket.getOutputStream(), true);
-            serverOutputStream.println("Server received message from : " + clientSocket.getInetAddress());
+            if(clientSocket == null){
+                serverOutputStream.println(0);
+            }
+            serverOutputStream.println(1);
+            System.out.println("Client successfully connected to server!" + clientSocket.getInetAddress());
+        } catch (IOException e) {
+            System.out.println("Unsuccessful connect to server, please disconnect " +
+                    "clientside and retry" + e.getMessage());
+        }
+    }
+
+    private static void sendMessage(Socket clientSocket, String message){
+        try {
+            PrintWriter serverOutputStream =
+                    new PrintWriter(clientSocket.getOutputStream(), true);
+            serverOutputStream.println(message);
         } catch (IOException e) {
             System.out.println("Error writing message to client!" + e.getMessage());
         }
