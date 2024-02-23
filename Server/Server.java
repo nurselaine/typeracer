edd7cfcf36d11d1770593eee983f243c3066e115
nurselaine <Elaine.t.huynh@gmail.com>
2024-02-22 23:21:16 -0800

    Refactored server program and added new client accept calls into thread

--- Server/Server.java
+++ Server/Server.java
@@ -8,11 +8,8 @@ import Server.Server_context.UserContext;
 
 import java.io.*;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
 
 public class Server{
@@ -27,28 +24,7 @@ public class Server{
 
         System.out.println("multi-threaded server...");
         int PORT = 3001;
-
-        // on server start = read user database and create new user profile for each saved user
-        Thread reloadClient = new Thread(() -> {
-            File userDB = new File("C:\\Users\\Elain\\Projects\\typeracer\\Server\\utils\\user_database.txt");
-            try {
-                Scanner fileReader = new Scanner(userDB);
-                while(fileReader.hasNextLine()){
-                    String[] user_credentials = fileReader.nextLine().split(" ");
-
-                    // get socket id - use localhost for development
-                    int colon = user_credentials[0].indexOf(':');
-                    String host = user_credentials[0].substring(0, colon);
-
-                    // create new user context and add to user cache
-                    userCache.addNewUser(new UserContext(host, user_credentials[1], user_credentials[2]));
-                }
-            } catch (IOException e){
-                System.out.println("Unable to initialize user database");
-                e.printStackTrace();
-            }
-        });
-        reloadClient.start();
+        initializeSavedUsers();
 
         try {
             ServerSocketService socketServer = new ServerSocketService(PORT);
@@ -58,57 +34,13 @@ public class Server{
             // Creating Semaphore object to share client reader and writers for connecting to server
             Semaphore sem = new Semaphore(4);
 
-
-            // Non-blocking socket that allows clients to be accepted and
             while(socketServer.isAccepting()){
 
-                // socket accepts 4 client request & creates client socket simultaneuously
-                Socket clientSocket = socketServer.acceptConnection();
+                Future<Socket> clientSocketFuture = connectRPC(socketServer, executorService, sem);
+                Socket clientSocket = clientSocketFuture.get();
 
-                // Use a thread from thread pool to notify client of connection status
-                // and listen to incoming client messages
-                executorService.execute(() -> {
+                handleClient(clientSocket);
 
-                    try {
-                        sem.acquire();
-
-                        // create a client reader and writer objects for each new thread created
-                        PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-                        BufferedReader clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        ConnectRPC(clientSocket, clientWriter, clientReader);
-
-                        sem.release();
-
-                    } catch (IOException | InterruptedException e){
-                        System.out.println("Unsuccessful connect to server, please disconnect " +
-                                "client-side and retry" + e.getMessage());
-                    }
-                });
-
-                clientThreadCount++;
-
-                // create and start client thread for each new accepted socket
-                Thread clientThread = new Thread(() -> {
-                    System.out.println("New client thread being created");
-                    try{
-
-                        // create individual client writer and reader object
-                        PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-                        BufferedReader clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                        // handle incoming requests and map client to the correct RPC
-                        receiveMessage(clientSocket, clientWriter, clientReader, "client" + clientThreadCount);
-                    } catch (IOException e){
-                        System.out.println("SERVER ERROR: ISSUES CREATING INDIVIDUAL CLIENT THREAD");
-                        e.printStackTrace();
-                    }
-                }, Integer.toString(clientThreadCount));
-                clientThread.start();
-//
-//                // wait for threads to join
-//                - this breaks the multithreaded server becuase it does not allow for mtliple clients to join
-//                clientThread.join();
-                clientThreadCount--;
             }
         } catch (Exception e) {
             System.out.println("Error starting server " + e.getMessage());
@@ -120,19 +52,70 @@ public class Server{
      * ConnectRPC will handle notifying clientside that client has been connected
      * succesfully and then transfer over to ClientHandler thread for auth RPCs
      * */
-    private static void ConnectRPC(Socket clientSocket, PrintWriter clientWriter, BufferedReader clientReader) {
-        if (clientSocket == null) {
-            clientWriter.println(0);
-            System.out.println("Client socket not connected to server!" + clientSocket.getInetAddress());
-        } else {
-            clientWriter.println(1);
-        }
+    private static Future<Socket> connectRPC(ServerSocketService socketServer, ExecutorService executorService, Semaphore sem) {
+        // Use a thread from thread pool to notify client of connection status
+        // and listen to incoming client messages
+        CompletableFuture<Socket> futureSocket = new CompletableFuture<>();
+        executorService.execute(() -> {
+            try {
+                Socket clientSocket = socketServer.acceptConnection();
+                sem.acquire();
+
+                // create a client reader and writer objects for each new thread created
+                PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                if (clientSocket == null) {
+                    clientWriter.println(0);
+                    System.out.println("Client socket not connected to server!" + clientSocket.getInetAddress());
+                } else {
+                    clientWriter.println(1);
+                    futureSocket.complete(clientSocket);
+                }
+
+                sem.release();
+                clientWriter.close();
+
+            } catch (IOException | InterruptedException e){
+                System.out.println("Unsuccessful connect to server, please disconnect " +
+                        "client-side and retry" + e.getMessage());
+            }
+        });
+        return futureSocket;
     }
 
+    /**
+     * Creates a new thread for every accepted client socket and handles input/output streams
+     * and client requests
+     * */
+    private static void handleClient(Socket clientSocket){
+        clientThreadCount++;
+
+        // create and start client thread for each new accepted socket
+        Thread clientThread = new Thread(() -> {
+            System.out.println("New client thread being created");
+            try{
+
+                // create individual client writer and reader object
+                PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader clientReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                // handle incoming requests and map client to the correct RPC
+                receiveMessage(clientSocket, clientWriter, clientReader, "client" + clientThreadCount);
+            } catch (IOException e){
+                System.out.println("SERVER ERROR: ISSUES CREATING INDIVIDUAL CLIENT THREAD");
+                e.printStackTrace();
+            }
+        }, Integer.toString(clientThreadCount));
+        clientThread.start();
+
+        clientThreadCount--;
+    }
+
+    /**
+     * Manages client requests and routes client to correct request handler
+     * */
     private static void receiveMessage(Socket clientSocket, PrintWriter clientWriter,
                                        BufferedReader clientReader, String clientName) throws IOException {
         try {
-//            System.out.println("receive message");
 
             String clientMessage;
             LoginRPC loginHandler = new LoginRPC(clientWriter, clientReader);
@@ -154,7 +137,7 @@ public class Server{
                     case "Disconnect":
                         break;
                     default:
-                        sendMessage(clientSocket, " Server received invalid client request.", clientWriter, clientReader);
+                        clientWriter.println(" Server received invalid client request.");
 
                 }
                 System.out.println("Message recieved from "+ clientName +"! " + clientMessage + " : ");
@@ -172,8 +155,32 @@ public class Server{
         }
     }
 
-    private static void sendMessage(Socket clientSocket, String message,  PrintWriter clientWriter, BufferedReader clientReader){
-        clientWriter.println(message);
+    /**
+     * Reads user database and creates new user contexts to add into user cache for
+     * users to skip the new user step each time the server shuts down
+     * */
+    private static void initializeSavedUsers(){
+        // on server start = read user database and create new user profile for each saved user
+        Thread reloadClient = new Thread(() -> {
+            File userDB = new File("C:\\Users\\Elain\\Projects\\typeracer\\Server\\utils\\user_database.txt");
+            try {
+                Scanner fileReader = new Scanner(userDB);
+                while(fileReader.hasNextLine()){
+                    String[] user_credentials = fileReader.nextLine().split(" ");
+
+                    // get socket id - use localhost for development
+                    int colon = user_credentials[0].indexOf(':');
+                    String host = user_credentials[0].substring(0, colon);
+
+                    // create new user context and add to user cache
+                    userCache.addNewUser(new UserContext(host, user_credentials[1], user_credentials[2]));
+                }
+            } catch (IOException e){
+                System.out.println("Unable to initialize user database");
+                e.printStackTrace();
+            }
+        });
+        reloadClient.start();
     }
 
 }
